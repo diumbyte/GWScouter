09c9361fc5de6b09599166bb37b382baba134f21
Diumbyte <diumbyte@gmail.com>
2022-02-26 23:46:00 -0500

    Fix env vars to use Heroku's dynamic database uri

--- config/prod.js
+++ config/prod.js
@@ -1,15 +1,6 @@
 module.exports = {
     discordClientId: process.env.DISCORD_CLIENT_ID,
     discordClientSecret: process.env.DISCORD_CLIENT_SECRET,
-    PGConnection: {
-        host: process.env.HEROKU_PG_DB_HOST,
-        user: process.env.HEROKU_PG_DB_USER,
-        port: process.env.HEROKU_PG_DB_PORT,
-        password: process.env.HEROKU_PG_DB_PW,
-        database: process.env.HEROKU_PG_DB_DB,
-        ssl: {
-            rejectUnauthorized: false
-        }
-    },
+    PGConnection: process.env.DATABASE_URL,
     cookieKey: process.env.COOKIE_KEY
 }
